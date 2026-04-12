0554104f17655f8b75c2ebb0094cbb413ca497cb
PortiaMosokotso <portiamosokotso@gmail.com>
2026-04-12 13:01:54 +0200

    Final Fixes

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,45 +1,39 @@
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, RouterLink, CommonModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
+export class Login implements OnInit {
   email = '';
   password = '';
-  isLoading = false;
-  errorMessage = '';
 
   constructor(
     private authService: AuthService,
     private router: Router
   ) {}
 
-  login(form: NgForm) {
-    this.errorMessage = '';
+  ngOnInit(): void {
+    localStorage.removeItem('token');
+  }
 
-    if (form.invalid) {
-      form.control.markAllAsTouched();
-      return;
-    }
-
-    this.isLoading = true;
-    const payload = { email: this.email, password: this.password };
-
+  login() {
+    const payload = {
+      email: this.email,
+      password: this.password
+    };
     this.authService.login(payload).subscribe({
       next: () => {
         this.router.navigate(['/dashboard']);
       },
       error: (err) => {
-        this.isLoading = false;
-        this.errorMessage = err.error?.message || 'Invalid email or password.';
+        alert(err.error);
       }
     });
   }
